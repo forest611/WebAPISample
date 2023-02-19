ac364e0108fc1477385508070cdec27297a91ea1
forest611 <forest611328@gmail.com>
2023-02-20 00:46:47 +0900

    MySQL繋がった！

---
+++ WebAPIServer/WebAPIServer/Controllers/BankController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebAPIServer.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebAPIServer.Controllers
+{
+    [Route("api/[controller]")]
+    public class BankController : Controller
+    {
+        // GET: api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{mcid}")]
+        public double Get(string mcid)
+        {
+            var balance = BankData.GetBank(mcid);
+
+            Console.WriteLine($"Get:{balance}");
+
+            return balance;
+        }
+    }
+}
+
---
+++ WebAPIServer/WebAPIServer/Models/Man10Bank.cs
@@ -0,0 +1,66 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Data.Common;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebAPIServer.Models
+{
+	/// <summary>
+	/// Man10Bankのテーブル構造
+	/// </summary>
+	public class UserBank
+	{
+		[Key]
+		public int id { get; set; }
+		public string player { get; set; }
+        public string uuid { get; set; }
+        public double balance { get; set; }
+
+    }
+
+	/// <summary>
+	/// 接続設定など
+	/// DbContextはスレッドセーフでない
+	/// </summary>
+	public class UserBankContext : DbContext
+	{
+
+		public DbSet<UserBank> user_Bank { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString = "server=localhost;port=3306;user=forest;password=rDcrmPRLJvu@ex/E,>K;database=man10_bank";
+
+            var serverVersion = new MySqlServerVersion(new Version(8, 0, 30));
+
+			optionsBuilder.UseMySql(connectionString,serverVersion);
+        }
+
+    }
+
+	/// <summary>
+	/// データ
+	/// </summary>
+	public class BankData
+	{
+
+		public static double GetBank(string mcid)
+		{
+
+			var db = new UserBankContext();
+
+			var balance = db.user_Bank.Where(t => t.player == mcid).FirstOrDefault()?.balance ?? 0.0;
+
+			db.user_Bank.ForEachAsync(result =>
+			{
+                Console.WriteLine($"FOREACH:{result.player}{result.balance}");
+            });
+
+            Console.WriteLine($"mcid:{mcid} balance:{balance}");
+
+			return balance;
+		}
+
+	}
+
+}
+
